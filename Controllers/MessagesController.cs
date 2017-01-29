d003a06ac6d6333ed54e02479746ed74581c0355
Alex Brisan <s1505883@ed.ac.uk>
2017-01-29 09:14:35 +0000

    Perhaps fixed now

--- Controllers/MessagesController.cs
+++ Controllers/MessagesController.cs
@@ -81,10 +81,10 @@ namespace KnowYourself
                             List<int> ids = questions.Keys.OrderBy(i => i).ToList();
                             string question ;
                             bool succ = questions.TryGetValue(ids.First(), out question);
-                            //currentState.SetProperty<int>("currentQuestionID", ids.First());
+                            currentState.SetProperty<int>("currentQuestionID", ids.First());
                             //currentState.SetProperty<int>("questionListIndex", 0);
-                            //currentState.SetProperty<bool>("answerQuestion", true);
-                            // await state.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, currentState);
+                            currentState.SetProperty<bool>("answerQuestion", true);
+                            await state.BotState.SetUserDataAsync(activity.ChannelId, activity.From.Id, currentState);
                             Activity resp = activity.CreateReply(question);
                             await connector.Conversations.ReplyToActivityAsync(resp);
                         }catch(Exception e)
